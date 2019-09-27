11a41627d8b1b19bc963382f43f5e802e2abf01d
pmomesso <pmomesso@itba.edu.ar>
2019-09-27 18:38:55 -0300

    Same as previous commit

--- persistence/src/main/java/ar/edu/itba/paw/persistence/SeriesDaoJdbc.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/SeriesDaoJdbc.java
@@ -236,7 +236,7 @@ public class SeriesDaoJdbc implements SeriesDao {
 
     private void addUserToPosts(List<Post> postList) {
         for(Post post : postList) {
-            post.setUserPoster(userDao.getUser(post.getUserId()));
+            post.setUserPoster(userDao.getUserById(post.getUserId()));
         }
     }
 
