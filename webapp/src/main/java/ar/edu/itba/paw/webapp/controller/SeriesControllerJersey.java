91b540acc94c4cc8d7887850951f4f25d2dcfc17
pmomesso <pmomesso@itba.edu.ar>
2020-02-04 16:35:04 -0300

    Deleted test echo controller

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/SeriesControllerJersey.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/SeriesControllerJersey.java
@@ -11,10 +11,7 @@ import ar.edu.itba.paw.webapp.dtos.SeriesReviewsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.print.attribute.standard.Media;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
