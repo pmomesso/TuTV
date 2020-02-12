c8395d783e0e51a8046c5bc55e40a00593a0539b
Franco <fbaliarda@itba.edu.ar>
2020-02-12 20:12:07 -0300

    Endpoints para ver temporada y episodio

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dtos/ViewedResourceDTO.java
@@ -0,0 +1,17 @@
+package ar.edu.itba.paw.webapp.dtos;
+
+public class ViewedResourceDTO {
+
+    private Boolean viewed;
+
+    public ViewedResourceDTO(){
+    }
+
+    public Boolean getViewed() {
+        return viewed;
+    }
+
+    public void setViewed(Boolean viewed) {
+        this.viewed = viewed;
+    }
+}
