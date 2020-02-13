3b95d901060b3b153853625b2bddbcc59f12a43e
Agustina Osimani <aosimani@itba.edu.ar>
2020-02-13 23:38:14 +0100

    wip profile

--- frontend/src/js/navigator.js
+++ frontend/src/js/navigator.js
@@ -1,22 +1,22 @@
 import $ from 'jquery';
 
-function extend() {
-    if ($(".page-left").hasClass("extended")) {
-        $(".page-left").removeClass("extended");
-    } else {
-        $(".page-left").addClass("extended");
-    }
-}
-
-function confirmAction(event, message) {
-    event.preventDefault();
-
-    var form = event.target;
-    var accept = window.confirm(message);
-
-    if (accept)
-        form.submit();
-}
+// function extend() {
+//     if ($(".page-left").hasClass("extended")) {
+//         $(".page-left").removeClass("extended");
+//     } else {
+//         $(".page-left").addClass("extended");
+//     }
+// }
+//
+// function confirmAction(event, message) {
+//     event.preventDefault();
+//
+//     var form = event.target;
+//     var accept = window.confirm(message);
+//
+//     if (accept)
+//         form.submit();
+// }
 
 $( document ).ready(function() {
 
@@ -27,13 +27,13 @@ $( document ).ready(function() {
 });
 
 
-function functionConfirm(msg, myYes, myNo) {
-    var confirmBox = $("#confirm");
-    confirmBox.find(".message").text(msg);
-    confirmBox.find(".yes,.no").unbind().click(function() {
-        confirmBox.hide();
-    });
-    confirmBox.find(".yes").click(myYes);
-    confirmBox.find(".no").click(myNo);
-    confirmBox.show();
-}
+// function functionConfirm(msg, myYes, myNo) {
+//     var confirmBox = $("#confirm");
+//     confirmBox.find(".message").text(msg);
+//     confirmBox.find(".yes,.no").unbind().click(function() {
+//         confirmBox.hide();
+//     });
+//     confirmBox.find(".yes").click(myYes);
+//     confirmBox.find(".no").click(myNo);
+//     confirmBox.show();
+// }
